b4537ad03d156fc81cf09d2180e54f5f61d2be44
zhangchangcheng <zhangchangcheng.banggood.cn>
2017-07-07 08:51:02 +0800

    initial commit

---
+++ src/components/seller/seller.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <div class="content-warpper">
+      <div class="avater">
+        <img :src="seller.avater" alt="" width="64" height="64">
+      </div>
+    </div>
+    <div class="content">
+      <div class="title">
+        <span class="brand"></span>
+        <span class="name">{{ seller.name }}</span>
+      </div>
+      <div class="descirption">
+        {{ seller.descirption}}/{{selelr.deliveryTime}}
+      </div>
+    </div>
+    <div class="bulletin-warpper">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  }
+}
+</script>
